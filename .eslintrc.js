ea4332bf12221d28f2a4252c99ea3216326553e3
Dmitriy <43067932+Krudda@users.noreply.github.com>
2020-04-11 15:48:08 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-multi-str': 'off',
+    "indent": [2, "tab", { "SwitchCase": 1 }],
+    "no-tabs": 0,
+    "no-unused-expressions": ["error", { "allowTernary": true }],
+    "no-param-reassign": ["error", { "props": false }],
+    "max-len": ["error", { "code": 150 }]
+  },
 };
